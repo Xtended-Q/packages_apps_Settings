e2e05de9c583df7aa28ceaaeeb115914a9286ba8
rayrzy <vincentrayananda@gmail.com>
2022-06-03 11:59:03 +0700

    Revert "BatteryHealth: Make it optional"
    This reverts commit 88703a1.
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: Ibf0c52c0b8f9cf09ccbd90b49bb54f461e0c29d1

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -287,22 +287,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         updateBatteryTipFlag(icicle);
         updateUltraPowerPrefs();
 
-        // Check availability of Battery Health
-        Preference mDesignedHealthPref = (Preference) findPreference(KEY_DESIGNED_BATTERY_CAPACITY);
-        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
-            getPreferenceScreen().removePreference(mDesignedHealthPref);
-        }
-
-        Preference mCurrentHealthPref = (Preference) findPreference(KEY_CURRENT_BATTERY_CAPACITY);
-        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
-            getPreferenceScreen().removePreference(mCurrentHealthPref);
-        }
-
-        Preference mCyclesHealthPref = (Preference) findPreference(KEY_BATTERY_CHARGE_CYCLES);
-        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
-            getPreferenceScreen().removePreference(mCyclesHealthPref);
-        }
-
         //Ultra Power Saving
         Preference mUltraPowerSavingPref = (Preference) findPreference(KEY_ULTRA_POWER_SAVING);
         if (!getResources().getBoolean(R.bool.config_supportUltraPowerSaving)) {
