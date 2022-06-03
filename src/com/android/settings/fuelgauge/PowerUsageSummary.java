e894e04d6d29867162ba72b828dbea4572d7434a
rayrzy <vincentrayananda@gmail.com>
2022-06-03 12:07:29 +0700

    Revert "BatteryHealth: make resources overlayable"
    This reverts commit 71e5327.
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: Ic02ddc433ac3add679fef64dccf6fbfb4bac999a

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -106,9 +106,12 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     private static final String KEY_BATTERY_SAVER_SUMMARY = "battery_saver_summary";
     private static final String KEY_ULTRA_POWER_SAVING = "ultra_power_save";
 
+    private static final String FILENAME_BATTERY_DESIGN_CAPACITY =
+            "/sys/class/power_supply/bms/charge_full_design";
+    private static final String FILENAME_BATTERY_CURRENT_CAPACITY =
+            "/sys/class/power_supply/bms/charge_full";
+
     private SystemSettingSwitchPreference mUltraPower;
-    private String mBatDesCap;
-    private String mBatCurCap;
 
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
@@ -435,17 +438,14 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
             mNeedUpdateBatteryTip = true;
         }
 
-        mBatDesCap = getResources().getString(R.string.config_batDesCap);
-        mBatCurCap = getResources().getString(R.string.config_batCurCap);
-
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
         updateLastFullChargePreference();
         mScreenUsagePref.setSubtitle(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
         mBatteryTempPref.setSubtitle(BatteryInfo.batteryTemp / 10 + " °C");
-        mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(mBatCurCap));
-        mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(mBatDesCap));
+        mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(FILENAME_BATTERY_CURRENT_CAPACITY));
+        mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(FILENAME_BATTERY_DESIGN_CAPACITY));
         final long elapsedRealtimeUs = SystemClock.elapsedRealtime() * 1000;
         Intent batteryBroadcast = context.registerReceiver(null,
                 new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
